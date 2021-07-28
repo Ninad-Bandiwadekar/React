43c7b3ba0ac7c1b9a3bb51ee7643d0f3b13f886b
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 02:39:16 +0530

    map.react

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let title="Hello";
+  let list=["Delhi","Calcutta","Mumbai","lonavala"]
+
   return (
-    <div className="App">
-      
-        Hello world!!!
+    <div>
+      <h1>{title}</h1>
+      <ul>
+       {list.map((item)=>{
+            return <h1>{item}</h1>;
+       })}
+       </ul>
     </div>
   );
 }
