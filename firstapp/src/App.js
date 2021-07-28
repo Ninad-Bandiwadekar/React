39a2f002c6c5e149aa3fa22cee5b18da54dfb1a5
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 02:59:22 +0530

    list

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title="Cdac";
+  let list=["mumbai","delhi","pune"];
+
   return (
     <div className="App">
-      
-        Hello world!!!
+      <h1>{title}</h1>
+      <h1>{list}</h1>
+       
     </div>
   );
 }
