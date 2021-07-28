03275a49edadae7cd4a03643e8ead5bf3d72dedc
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 03:29:19 +0530

    list and event

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -2,14 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let list=["hello"];
   
   let myFun=()=>{
-    alert("Event is called");
+    list.push("Hello");
+
+    console.log(list);
+    
   }
+
+  //View
+  //react is unaware of the changes of local variable
   return (
     <div>
-      <h1>Event Demo</h1>
-      <input type="button" value="Click Me" onClick={myFun} />
+      <h1>Working With Event and List</h1>
+      <input type="button" value="Click Me" onClick={myFun} />   
+
+       {list.map((item)=>{             ///map to retrive data
+          return <h3>{item}</h3>;
+       })}      
     </div>
   );
 }
