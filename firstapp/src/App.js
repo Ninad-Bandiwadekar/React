7af48b6476b20347a74bec81fcdb05e0d0e5380f
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 14:18:35 +0530

    counter

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,28 +4,21 @@ import { useState } from "react";
 
 function App() {
   //Stateless member
- let [counter]=useState(100);
+ let [counter,setCounter]=useState(100);
 
- let[name]=useState("cdac");
- let[list]=useState("delhi")
- let[std1]=useState({name:"rahul"});
- let[active]=useState(true);
+
 
  let increment=()=>{
    counter+=1;
-    
+    setCounter(counter);
    console.log(counter);
  }
 
   return (
     <div>
-      <h1 className="bg-dark text-light p-3">Statefull Update</h1>
-       <h1>String::{name}</h1>
-       <h1>Number ::{counter}</h1>
-       <h1>Boolean::{active+""}</h1>
-       <h1>List::{list}</h1>
-       <h1>Object::{std1.name}</h1>
-       <h1>Object::{JSON.stringify(std1)}</h1>
+      <h1 className="bg-dark text-light p-3">Statefull Event</h1>
+      <h1> Counter::{counter}</h1>
+       <input type="button" value="Increment" onClick={increment}/>
     </div>
   );
 }
