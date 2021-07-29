797df90ec4c5619252e4974f91f48e6dcc9b2554
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 14:34:42 +0530

    statefull list

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,21 +4,32 @@ import { useState } from "react";
 
 function App() {
   //Stateless member
- let [counter,setCounter]=useState(100);
+  let [counter, setCounter] = useState(100);
+  let [list, setlist] = useState(["delhi", "Mumbai"]);
 
-
-
- let increment=()=>{
-   counter+=1;
+  let increment = () => {
+    counter += 1;
     setCounter(counter);
-   console.log(counter);
- }
+    console.log(counter);
+  };
 
+
+  let addNewElement=()=>{
+    let newList=["Kolkata",...list];
+    setlist(newList);
+
+  }
   return (
     <div>
-      <h1 className="bg-dark text-light p-3">Statefull Event</h1>
+      <h1 className="bg-dark text-light p-3">Statefull List Event</h1>
       <h1> Counter::{counter}</h1>
-       <input type="button" value="Increment" onClick={increment}/>
+      <input type="button" value="Increment" onClick={increment} />
+      <hr />
+
+      <input type="button" value="Add New List" onClick={addNewElement} />
+      {list.map((item) => {
+        return <h1>{item}</h1>;
+      })}
     </div>
   );
 }
