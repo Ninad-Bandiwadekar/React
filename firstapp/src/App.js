6145a3edc9f92abfeca6851f9065670591399c2f
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 19:09:52 +0530

    linking

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -3,18 +3,26 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { HelloWorld } from "./pages/HelloWorld";
 import { HelloUniverse } from "./pages/HelloUniverse";
-import{BrowserRouter,Route} from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-       <BrowserRouter>
-       <Route exact={true} path="/page1"component={Page1}/>
-       <Route exact={true} path="/page2"component={Page2}/>
-       <Route exact={true} path="/page3"component={Page3}/>
-       <Route exact={true} path="/"component={Page1}/>
-       </BrowserRouter>
+    <BrowserRouter>
+      <Link to="/page1">
+        <h1>Page1</h1>
+      </Link>
+      <Link to="/page2">
+        <h1>Page2</h1>
+      </Link>
+      <Link to="/page3">
+        <h1>Page3</h1>
+      </Link>
+      <Route exact={true} path="/page1" component={Page1} />
+      <Route exact={true} path="/page2" component={Page2} />
+      <Route exact={true} path="/page3" component={Page3} />
+      <Route exact={true} path="/" component={Page1} />
+    </BrowserRouter>
   );
-  
 }
 function Page1() {
   return (
@@ -22,9 +30,9 @@ function Page1() {
       <h1>Page1</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
-        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus
-        natus facilis commodi necessitatibus quaerat molestias nihil sint
-        ratione, laboriosam architecto dicta?
+        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus natus
+        facilis commodi necessitatibus quaerat molestias nihil sint ratione,
+        laboriosam architecto dicta?
       </p>
     </div>
   );
@@ -35,9 +43,9 @@ function Page2() {
       <h1>Page2</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
-        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus
-        natus facilis commodi necessitatibus quaerat molestias nihil sint
-        ratione, laboriosam architecto dicta?
+        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus natus
+        facilis commodi necessitatibus quaerat molestias nihil sint ratione,
+        laboriosam architecto dicta?
       </p>
     </div>
   );
@@ -48,9 +56,9 @@ function Page3() {
       <h1>Page3</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
-        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus
-        natus facilis commodi necessitatibus quaerat molestias nihil sint
-        ratione, laboriosam architecto dicta?
+        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus natus
+        facilis commodi necessitatibus quaerat molestias nihil sint ratione,
+        laboriosam architecto dicta?
       </p>
     </div>
   );
