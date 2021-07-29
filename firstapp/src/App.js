72a3c2c647659974f2ff26bcaf1fd306e1dd52e6
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-30 01:36:45 +0530

    static data

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -7,70 +7,47 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <NavMenu/>
-      <Route exact={true} path="/page1" component={Page1} />
-      <Route exact={true} path="/page2" component={Page2} />
-      <Route exact={true} path="/page3" component={Page3} />
-      <Route exact={true} path="/hw" component={HelloWorld} />
-      <Route exact={true} path="/" component={Page1} />
-    </BrowserRouter>
+    <div>
+      <MyComponent />
+    </div>
   );
 }
-function NavMenu(){ return(
-  <div>
-<Link to="/page1">
-        <h1>Page1</h1>
-      </Link>
-      <Link to="/page2">
-        <h1>Page2</h1>
-      </Link>
-      <Link to="/page3">
-        <h1>Page3</h1>
-      </Link>
-      <Link to="/hw">
-        <h1>HelloWorld</h1>
-      </Link>
-      </div>
-)}
+function MyComponent() {
+  let [list, setList] = useState(["delhi"]);
 
-function Page1() {
+  const postYourThought = () => {
+    const newlist = ["New thought", ...list];
+    setList(newlist);
+  };
+
   return (
-    <div>
-      <h1>Page1</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
-        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus natus
-        facilis commodi necessitatibus quaerat molestias nihil sint ratione,
-        laboriosam architecto dicta?
-      </p>
+    <div className="m-2">
+      <h1>Work With Forms</h1>
+
+      <input
+        type="text"
+        placeholder="Post your thoughts"
+        className="form-control form-control-lg my-2"
+        style={{ height: 75 }}
+      />
+      <input
+        type="button"
+        value="Post your thought"
+        className="btn btn-primary w-100"
+        onClick={postYourThought}
+      />
+
+      <div>
+        <div className="h1 bg-light my-1 p-3 text-primary border">
+          Thought List
+        </div>
+
+        {list.map((item) => {
+          return <div className="alert alert-primary mt-1">{item}</div>;
+        })}
+      </div>
     </div>
   );
 }
-function Page2() {
-  return (
-    <div>
-      <h1>Page2</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
-        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus natus
-        facilis commodi necessitatibus quaerat molestias nihil sint ratione,
-        laboriosam architecto dicta?
-      </p>
-    </div>
-  );
-}
-function Page3() {
-  return (
-    <div>
-      <h1>Page3</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
-        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus natus
-        facilis commodi necessitatibus quaerat molestias nihil sint ratione,
-        laboriosam architecto dicta?
-      </p>
-    </div>
-  );
-}
+
 //export default App;
