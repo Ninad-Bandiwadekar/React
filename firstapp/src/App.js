3526a51949ef49bef4d886eee773eb6fcec19075
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 18:25:01 +0530

    app.js as user define tag

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -2,23 +2,21 @@ import "./App.css";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div>
-      <h1 className="bg-dark text-light p-3">Working with css Again</h1>
-      <h5 style={{ color: "tomato", backgroundColor: "royalblue" }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid porro
-        rerum vitae maxime itaque ad. Illo omnis sed quos velit quis earum eaque
-        laborum! Unde odio culpa dolore velit ullam?
-      </h5>
+//we are creating user define tag
+//<App/>- it should be mentioned like these
+/*
+when finction is returning tag it is a user define tag
 
-      <p className="beautify">
-         Css fileLorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        nobis laboriosam nostrum suscipit quo reprehenderit fuga hic accusantium
-        delectus sequi et, voluptate maxime! Sapiente ad odio amet autem ab?
-        Quis?
-      </p>
-    </div>
-  );
+pre define tag
+<h1></h1>
+<a>
+*/
+
+export default function App() {
+  return (
+    <div></div>
+  )
+    
+  
 }
-export default App;
+//export default App;
