f609eaf0c8fdda1f1396289d5fd8058ff227c8e6
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 19:04:10 +0530

    route

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,18 +1,58 @@
 import "./App.css";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import {HelloWorld}from './pages/HelloWorld';
-import {HelloUniverse}from './pages/HelloUniverse';
+import { HelloWorld } from "./pages/HelloWorld";
+import { HelloUniverse } from "./pages/HelloUniverse";
+import{BrowserRouter,Route} from "react-router-dom";
 
 export default function App() {
   return (
+       <BrowserRouter>
+       <Route exact={true} path="/page1"component={Page1}/>
+       <Route exact={true} path="/page2"component={Page2}/>
+       <Route exact={true} path="/page3"component={Page3}/>
+       <Route exact={true} path="/"component={Page1}/>
+       </BrowserRouter>
+  );
+  
+}
+function Page1() {
+  return (
     <div>
-      <HelloWorld />
-      <HelloWorld />
-
-      <HelloUniverse />
+      <h1>Page1</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
+        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus
+        natus facilis commodi necessitatibus quaerat molestias nihil sint
+        ratione, laboriosam architecto dicta?
+      </p>
     </div>
   );
-
-  }
+}
+function Page2() {
+  return (
+    <div>
+      <h1>Page2</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
+        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus
+        natus facilis commodi necessitatibus quaerat molestias nihil sint
+        ratione, laboriosam architecto dicta?
+      </p>
+    </div>
+  );
+}
+function Page3() {
+  return (
+    <div>
+      <h1>Page3</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis
+        sequi saepe incidunt voluptate facilis magni eaque nihil! Delectus
+        natus facilis commodi necessitatibus quaerat molestias nihil sint
+        ratione, laboriosam architecto dicta?
+      </p>
+    </div>
+  );
+}
 //export default App;
