e38340656942cd005f132645fee50d58007b689b
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-29 14:11:44 +0530

    state full variable

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,19 +4,28 @@ import { useState } from "react";
 
 function App() {
   //Stateless member
- let counter=100;
+ let [counter]=useState(100);
+
+ let[name]=useState("cdac");
+ let[list]=useState("delhi")
+ let[std1]=useState({name:"rahul"});
+ let[active]=useState(true);
 
  let increment=()=>{
    counter+=1;
-
+    
    console.log(counter);
  }
 
   return (
     <div>
-      <h1 className="bg-dark text-light p-3">Statless Does not Update Ui</h1>
-       {counter}
-       <input type="button" value="Increment" onClick={increment}/>
+      <h1 className="bg-dark text-light p-3">Statefull Update</h1>
+       <h1>String::{name}</h1>
+       <h1>Number ::{counter}</h1>
+       <h1>Boolean::{active+""}</h1>
+       <h1>List::{list}</h1>
+       <h1>Object::{std1.name}</h1>
+       <h1>Object::{JSON.stringify(std1)}</h1>
     </div>
   );
 }
