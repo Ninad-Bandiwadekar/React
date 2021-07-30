4e3abcdf20933d2529ef22d8c71687e2fe804606
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-30 14:03:21 +0530

    prototype

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,28 +1,83 @@
+import logo from "./logo.svg";
 import "./App.css";
-import { useDebugValue, useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div>
-      <MyComponent />
+      <MyRegisterComponent />
     </div>
   );
 }
 
-function MyComponent() {
-  const handleClick = (e) => {
-    console.log(e);
-  };
-
+function MyRegisterComponent() {
   return (
     <div>
-      <h1>hello</h1>
+      <h1 className="bg-dark text-light p-3 ">User Registeation </h1>
 
-      <input type="button" value="Click Me" onClick={handleClick} />
-      <input type="button" value="Click Me1" onClick={(e) => handleClick(e)} />
+      {/** FORM COMPONENT */}
+      <form className="m-2">
+        <div>
+          <input
+            type="text"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter username"
+          />
+        </div>
 
-      <input type="button" value="Click Me2" onClick={(e) => handleClick(e,100)} />
+        <div>
+          <input
+            type="password"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter Passwword"
+          />
+        </div>
+
+        <div>
+          <input
+            type="email"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter Email"
+          />
+        </div>
+
+        <div>
+          <input
+            type="mobile"
+            className="form-control form-control-lg mb-1"
+            placeholder="Enter Mobile"
+          />
+        </div>
+
+        <div>
+          <input
+            type="button"
+            value="Register"
+            className="btn btn-lg btn-secondary w-100"
+          />
+        </div>
+      </form>
+
+      {/** List BOX HERE */}
+      <table className="table table-dark table-striped m-2">
+        <thead>
+          <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">USERNAME</th>
+            <th scope="col">PASSWORD</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">MOBILE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Rahul</td>
+            <td>@#@#@#@#</td>
+            <td>rahul@mgmil.com</td>
+            <td>121212</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
