51fb6f2271d85dea3633b0c02cd303596e156da4
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-30 13:31:34 +0530

    handling defalut props

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div>
+      <MyComponent/> {/* if not passed*/}
       <MyComponent heading="Java" desc="Java is cool"/>
       <MyComponent heading="JavaScript" desc="Javascript is super cool"/>
       <MyComponent heading="Python" desc="It is easy"/>
@@ -14,7 +15,7 @@ function App() {
 
 
 //Reading the props passed by parent.
-function MyComponent({heading,desc}) {
+function MyComponent({heading="Default heading",desc="descr not passed"}) {
   return (
     <div className="bg-success p-3 text-light">
       <h1>Learning {heading} </h1>
