99d251c0a7243c99a6d273bf355b21c790d946bd
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-30 13:37:59 +0530

    basic of event handling

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -2,32 +2,25 @@ import "./App.css";
 import { useDebugValue, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+export default function App() {
   return (
     <div>
-      <MyComponent/> {/* if not passed*/}
-      <MyComponent heading="Java" desc="Java is cool"/>
-      <MyComponent heading="JavaScript" desc="Javascript is super cool"/>
-      <MyComponent heading="Python" desc="It is easy"/>
+      <MyComponent />
     </div>
   );
 }
 
+function MyComponent() {
+  const handleClick = (e) => {
+    console.log(e);
+  };
 
-//Reading the props passed by parent.
-function MyComponent({heading="Default heading",desc="descr not passed"}) {
   return (
-    <div className="bg-success p-3 text-light">
-      <h1>Learning {heading} </h1>
-      <hr />
-      <p>
-       <mark className="rounded ">{desc}</mark> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni modi
-        culpa nulla mollitia explicabo unde tenetur similique fuga consequuntur
-        iusto cumque expedita ipsum, itaque ab odit dolorum delectus veniam
-        pariatur!
-      </p>
+    <div>
+      <h1>hello</h1>
+
+      <input type="button" value="Click Me" onClick={handleClick} />
+
     </div>
   );
 }
-
-export default App;
