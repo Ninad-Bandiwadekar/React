e06f621106e01fafad6f3c89808f5ec60951de74
Ninad-Bandiwadekar <ninadbandiwadekar18@gmail.com>
2021-07-30 13:27:55 +0530

    props cocept

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,80 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+import { useDebugValue, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { HelloWorld } from "./pages/HelloWorld";
-import { HelloUniverse } from "./pages/HelloUniverse";
-import { BrowserRouter, Route, Link } from "react-router-dom";
 
-export default function App() {
+function App() {
   return (
     <div>
-      <MyComponent />
+      <MyComponent heading="Java" desc="Java is cool"/>
+      <MyComponent heading="JavaScript" desc="Javascript is super cool"/>
+      <MyComponent heading="Python" desc="It is easy"/>
     </div>
   );
 }
-function MyComponent() {
-  let [list, setList] = useState([]);
 
-  //1
-  let[thought,setThought]=useState("");
 
-  const postYourThought = () => {
-    //const newlist = ["New thought", ...list];
-    const newlist=[thought,...list];
-    setList(newlist);
-
-    //clear the input:thought
-    setThought("");
-  };
-
-  //4 
-  const handleInputChange=(e)=>{
-    console.log(e.target.value);
-
-    const newthought=e.target.value;
-    setThought(newthought);
-
-  }
-
-const handleKeyEvent=(e)=>{
-  // console.log(e.key,e.keyCode);  //to find key code
-  if(e.keyCode==13){  //key code of enter
-    // console.log("User has pressed entered")
-   postYourThought();   //calling the function
-  }
-}
-
+//Reading the props passed by parent.
+function MyComponent({heading,desc}) {
   return (
-    <div className="m-2">
-      <h1>Work With Mini Facebook</h1>
-
-      <input
-        type="text"
-        style={{ height: 75 }}
-        placeholder="Post your thoughts"
-        className="form-control form-control-lg my-2"
-        value={thought} //2
-        onChange={handleInputChange}  //3
-        onKeyDown={handleKeyEvent}  //Enter-1
-      />
-      <input
-        type="button"
-        value="Post your thought"
-        className="btn btn-primary w-100"
-        onClick={postYourThought}
-      />
-
-      <div>
-        <div className="h1 bg-light my-1 p-3 text-primary border">
-          Thought List
-        </div>
-
-        {list.map((item) => {
-          return <div className="alert alert-primary mt-1">{item}</div>;
-        })}
-      </div>
+    <div className="bg-success p-3 text-light">
+      <h1>Learning {heading} </h1>
+      <hr />
+      <p>
+       <mark className="rounded ">{desc}</mark> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni modi
+        culpa nulla mollitia explicabo unde tenetur similique fuga consequuntur
+        iusto cumque expedita ipsum, itaque ab odit dolorum delectus veniam
+        pariatur!
+      </p>
     </div>
   );
 }
 
-//export default App;
+export default App;
